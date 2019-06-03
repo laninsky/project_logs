6e499ca98fd3f001b7df26906746f2421bafe738
Alana Alexander <laninsky@gmail.com>
2019-06-04 11:31:05 +1200

    Update sample_selection.R

--- hectors_hologenome/sample_selection.R
+++ hectors_hologenome/sample_selection.R
@@ -1,1 +1,5 @@
+### Description of data sets that were reconciled across
 
+# All data sets were delivered in excel format. I saved all of them as tab delimited for importing into R
+
+Here it is at last! I started with the most recent post mortem cases and worked backwards, with hopefully enough extras in there to counteract any that have to be left out due to lack of samples etc. I’ve removed the cases that didn’t have a solid diagnosis (eg due to decomposition), so what’s left are those with a good degree of confidence.
