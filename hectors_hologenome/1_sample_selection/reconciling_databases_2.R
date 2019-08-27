52a4eb4998f7eb1c176b10edd3d482f6a87a4ad3
laninsky <laninsky@gmail.com>
2019-08-27 14:15:21 +1200

    pulled out our sample-selection info and reconciliatio comments for the next script

---
+++ hectors_hologenome/1_sample_selection/reconciling_databases_2.R
@@ -0,0 +1,49 @@
+#### 8. Jettisoning the apparent duplicates before matching up to Pathology_data
+
+
+
+
+# Leaving reconciliation_notes for the end because these are contextual
+
+
+
+# Population broad_location based on sample code where this is known e.g. BP = Banks Peninsula, KK = Kaikoura, TI = Timaru
+
+# Ditch duplicates before combining with Pathology data
+
+
+# EXCLUDE "Cephalorhynchus hectori ?"
+
+# PRIORITIZE BECTORS (GIVES US AN IDEA OF ENVIRONMENTAL VS INHERITED) (AND HECTORS IN RANGE OF MAUI E.G. CHE12NZ02 - these are bectors -- There were two samples that have been classified as Hector's, but sampled in the range of the Māui (CHE11NZ06 and CHE12NZ02)
+# PRIORITIZE SAMPLED BY BIOPSY AND STRANDING (COMPARATIVE)
+# PRIORITIZE KNOWN DEATHS
+# INCLUDE BIOPSIES OF SIMILAR AGES TO STRANDED SAMPLES FOR LIVE VS DEATH COMPARISON
+# BALANCED SAMPLING NORTH ISLAND, NORTH COAST SOUTH ISLAND, WEST COAST SOUTH ISLAND, EAST COAST SOUTH ISLAND, SOUTH COAST SOUTH ISLAND
+# MTDNA/MICROSATELLITES AVAILABLE (MAKES IT MORE LIKELY IT IS A VIABLE SAMPLE/DOUBLE CHECK FOR SAMPLE MIX UPS)
+# MHC available
+# LARGE NUMBER OF MICROSATELLITES LOCI BETTER THAN LESS?
+# AGE CLASS?
+
+# Potentially don't want the Checked=="No" sample. Don't want Common_name=="Uncertain"
+# Date_received=="Nov-05" might be interesting - all caught in same net
+
+# Double check no problems in the reconciliation_notes after high-grading for samples.
+
+# "CHE05NZ18" has more info in it from Massey, so suggest taking it as the "sample of reference", but also referring to 'Che05NZ08' for additional/duplicate tissue if needed. 
+# Question: how independent are these likely to be if given a different code?
+
+# "CHE10NZ04" has more info in it from Massey, so suggest taking it as the "sample of reference", but also referring to 'CHE10NZ10' for additional/duplicate tissue if needed. Will also update the NAs for
+
+# bectors 1 =  NI10-03, Bectors2 = Chem15NZ08 and Chem16NZ31, Bectors 3 = NI10-24, NI11-11 and Chem15NZ04
+
+# Neils Beach Jackson Bay, South Westland could be interesting samples ( four co-caught dolphins)
+
+# Would the parent/fetus pairs be of any interest?
+
+# Museum samples to look at genomic loss of diversity?
+
+#DOC_Massey_database ($DOC_incident_no) should be able to be massaged to match up to Pathology_data ($H_no.)
+
+#Maui_recapture should be able to be matched up to DOC_Massey_database ($UoA_Code) and NZCETA_archive ($Code) based on $Indiv_ID
+
+
