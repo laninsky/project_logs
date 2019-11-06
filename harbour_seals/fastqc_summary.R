fc47fd6e47e8b951c40cf832cf726e1093d5553e
Alana Alexander <laninsky@gmail.com>
2019-11-06 21:20:23 +1300

    Update fastqc_summary.R

--- harbour_seals/fastqc_summary.R
+++ harbour_seals/fastqc_summary.R
@@ -4,5 +4,7 @@ if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
 # Reading in the fastqc summary
 fastqc <- read_tsv(fastqc_summary.txt,col_names=FALSE)
 
-# Creating a sample column
-fastqc %>% mutate(Sample=gsub("_L00.*","",X3)) %>% mutate(Read_file=gsub(".trimmed","",gsub(".fastq.gz","",X3)))
+# Creating columns so that we can carry out comparisons
+fastqc <- fastqc %>% mutate(Sample=gsub("_L00.*","",X3)) %>% 
+  mutate(Read_file=gsub(".trimmed","",gsub(".fastq.gz","",X3))) %>% 
+    mutate(Trimmed=ifelse(grepl("trimmed",X3),"Y","N"))
