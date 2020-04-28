5a6c354a2591cae976078781223c5a3a8780d581
Alana Alexander <laninsky@gmail.com>
2020-04-28 15:13:16 +1200

    Update fastqc_summary.R

--- random/fastqc_summary.R
+++ random/fastqc_summary.R
@@ -4,7 +4,48 @@ if (!require('tidyverse')) install.packages('tidyverse'); library('tidyverse')
 # Reading in the fastqc summary
 fastqc <- read_tsv("fastqc_summary.txt",col_names=FALSE)
 
-# Creating columns so that we can carry out comparisons
+# Reading in the key.txt file
+key <- read_table2("key.txt",col_names=FALSE)
+
+# Creating variables to capture sample name and whether file is R1 or R2
+Sample <- rep(NA,dim(fastqc)[1])
+R1_R2 <-  rep(NA,dim(fastqc)[1])
+undertermined_rows <- NULL
+
+# Working out these values for each of the rows in fastqc
+for (i in 1:length(Sample)) {
+  # If the file name in fastqc has "trimmed" in it
+  if (grepl("trimmed",fastqc$X3[i])) {
+    # If this file name has ".trimmed.R1.fastq.gz" in it
+    if (grepl(".trimmed.R1.fastq.gz",fastqc$X3[i])) {
+      # The R1_R2 should equal "R1"
+      R1_R2[i] <- "R1"
+      # And Sample should equal file name with ".trimmed.R1.fastq.gz" gsubbed off
+      Sample[i] <- gsub(".trimmed.R1.fastq.gz","",fastqc$X3[i])
+    } else {
+      # Otherwise if the file name has "trimmed" but not ".trimmed.R1.fastq.gz" in it
+      # R1_R2 should be "R2"
+      R1_R2[i] <- "R2"
+      # And Sample should be the file name with ".trimmed.R2.fastq.gz" gsubbed off
+      Sample[i] <- gsub(".trimmed.R2.fastq.gz","",fastqc$X3[i])
+    }     
+  } else {
+    if (length((which(key$X1 %in% fastqc$X3[i])))==0) {
+      undertermined_rows <- c(undertermined_rows,i)
+    } else {  
+      Sample[i] <- as.matrix(key[(which(key$X1 %in% fastqc$X3[i])),2])[1,1]
+      if (grepl("_R1_001.fastq.gz",fastqc$X3[i])) {
+        R1_R2[i] <- "R1"
+      } else {
+        R1_R2[i] <- "R2"
+      }
+    }
+  }
+}  
+
+# Removing rows not present in the key.txt file
+fastqc <- fastqc[-undertermined_rows,]
+
 fastqc <- fastqc %>% mutate(Sample=gsub("_L00.*","",X3)) %>% 
   mutate(Read_file=gsub(".trimmed","",gsub(".fastq.gz","",X3))) %>% 
     mutate(Trimmed=ifelse(grepl("trimmed",X3),"Y","N"))
