46a2dab89e67a1370a3edac8bcc5f7492feb4d1c
Alana Alexander <laninsky@gmail.com>
2020-05-06 14:35:25 +1200

    Update S4.R
    Swapping out S2 for Table_S1

--- chickadee/scripts/S4.R
+++ chickadee/scripts/S4.R
@@ -19,7 +19,7 @@ library(ggrepel)
 setwd("chickadee/output/")
 
 # 3. Reading in data (tab delimited), dropping last blank row
-temp <- read_tsv("../data/S2.txt")
+temp <- read_tsv("../data/Table_S1.txt")
 temp <- temp[1:165,]
 
 # 4. Creating a new table with the variables we are interested in
