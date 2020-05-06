7201542c3b256a2ffafe8563f17ce77ae7dff6d3
Alana Alexander <laninsky@gmail.com>
2020-05-06 14:31:04 +1200

    Update stats_presented_in_text.R
    Replacing S2 with Table_S1

--- chickadee/scripts/stats_presented_in_text.R
+++ chickadee/scripts/stats_presented_in_text.R
@@ -5,7 +5,7 @@ library(tidyverse)
 setwd("chickadee/output/")
 
 # 3. Reading in data (tab delimited), dropping last blank row
-temp <- read_tsv("../data/S2.txt")
+temp <- read_tsv("../data/Table_S1.txt")
 temp <- temp[1:165,]
 
 #4. Appleton City genetic cluster temporal comparisons
