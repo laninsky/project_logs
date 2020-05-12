acb33a003977de14ca6aaedbc6252a850e6fecfb
laninsky <laninsky@gmail.com>
2020-05-12 21:46:27 +1200

    Making some modifications due to the switch to the black-capped chickadee genome

--- chickadee/scripts/Fig_S7b_generate_bgc_inputs.R
+++ chickadee/scripts/Fig_S7b_generate_bgc_inputs.R
@@ -4,7 +4,8 @@
 
 # Necessary files 
 # headerless.vcf: to obtain chromosome, and position along chromosome
-# ref_guided.snps.map: in order to find which SNP positions correspond to each locus
+# *.snps.map or *.snpsmap from ipyrad outfiles: in order to find which SNP 
+# positions correspond to each locus (in this case chickadee_ref.snpsmap )
 # chromosome_scaffolds.txt: to find which scaffold corresponds to what chromosome
 # popmap.txt: will use this file (and Table_S1.txt) to divide up birds into "blackcapped, "Carolina", and "admixed"
 # The file "popmap.txt" was created by running the following code in the ipyrad directory
@@ -15,6 +16,8 @@
 # required structure assignment to BC to be considered "pure BC"
 structure_thres <- 0.99
 # required completeness of loci to be included
+# this should be adjusted so that the bgc run can complete before
+# the wall time is hit
 complete_thres <- 0.90
 # Header rows of vcf says vcf nucleotide order is CATG
 nucleotide_order <- c("C","A","T","G")
@@ -112,7 +115,7 @@ vcf <- vcf %>% group_by(locus) %>% filter(missing_data==min(missing_data))
 no_indivs <- dim(vcf)[2]-12+1
 vcf <- vcf %>% filter(missing_data<=(no_indivs*(1-complete_thres)))
 
-# Taking the SNP with the largest MAF
+# Taking the SNP with the largest MAF per locus
 maf <- rep(NA,dim(vcf)[1])
 for (i in 1:dim(vcf)[1]) {
   print(paste("Up to ",i," out of ",dim(vcf)[1], " SNPs",sep=""))
@@ -136,12 +139,12 @@ vcf <- vcf %>% group_by(locus) %>% slice(.,1)
 # which gives a numerical number for each scaffold (required for bgc)
 chrom_scaff <- read_tsv("chromosome_scaffolds.txt",col_names = FALSE)
 
-# The grep pattern on the gff file failed to pull out the mtDNA scaffold.
-# Here we are finding the "missing" scaffold number from the vcf file that
-# corresponds to the mtDNA, so we can add it to chrom_scaff
-temprow <- c(as.matrix(unique(vcf$`#CHROM`))[which(!(as.matrix(unique(vcf$`#CHROM`)) %in% as.matrix(chrom_scaff$X1)))],"mtDNA")
-names(temprow) <- names(chrom_scaff)
-chrom_scaff <- bind_rows(chrom_scaff,temprow)
+# If using a gff file, the  grep pattern will fail to pull out the mtDNA 
+# scaffold. This code will find the "missing" scaffold number from the vcf file 
+# that corresponds to the mtDNA, so we can add it to chrom_scaff
+# temprow <- c(as.matrix(unique(vcf$`#CHROM`))[which(!(as.matrix(unique(vcf$`#CHROM`)) %in% as.matrix(chrom_scaff$X1)))],"mtDNA")
+# names(temprow) <- names(chrom_scaff)
+# chrom_scaff <- bind_rows(chrom_scaff,temprow)
 
 bgc_chrom <- seq(1,(dim(chrom_scaff)[1]))
 bgc_chrom <- as_tibble(bgc_chrom)
@@ -256,12 +259,12 @@ vcf_filtered <- vcf %>% select(-bgc_chrom,-maf,-missing_data,-locus)
 write.table(vcf_filtered,"filtered_headerless.vcf",quote = FALSE,col.names = TRUE,row.names = FALSE)
 
 sessionInfo()
-#R version 3.5.3 (2019-03-11)
+#R version 3.6.2 (2019-12-12)
 #Platform: x86_64-pc-linux-gnu (64-bit)
 #Running under: CentOS Linux 7 (Core)
 
 #Matrix products: default
-#BLAS/LAPACK: /scale_wlg_persistent/filesets/opt_nesi/CS400_centos7_bdw/imkl/2018.4.274-gimpi-2018b/compilers_and_libraries_2018.5.274/linux/mkl/lib/intel64_lin/libmkl_gf_lp64.so
+#BLAS/LAPACK: /scale_wlg_persistent/filesets/opt_nesi/CS400_centos7_bdw/imkl/2020.0.166-gimpi-2020a/compilers_and_libraries_2020.0.166/linux/mkl/lib/intel64_lin/libmkl_gf_lp64.so
 
 #locale:
 #  [1] LC_CTYPE=en_US.UTF-8       LC_NUMERIC=C              
@@ -275,18 +278,20 @@ sessionInfo()
 #  [1] stats     graphics  grDevices utils     datasets  methods   base     
 
 #other attached packages:
-#  [1] forcats_0.4.0   stringr_1.4.0   dplyr_0.8.3     purrr_0.3.3    
-#[5] readr_1.3.1     tidyr_1.0.0     tibble_2.1.3    ggplot2_3.2.1  
-#[9] tidyverse_1.2.1
+#  [1] forcats_0.4.0   stringr_1.4.0   dplyr_0.8.4     purrr_0.3.3    
+#[5] readr_1.3.1     tidyr_1.0.2     tibble_2.1.3    ggplot2_3.2.1  
+#[9] tidyverse_1.3.0
 
 #loaded via a namespace (and not attached):
-#  [1] Rcpp_1.0.1       cellranger_1.1.0 pillar_1.4.2     compiler_3.5.3  
-#[5] tools_3.5.3      zeallot_0.1.0    jsonlite_1.6     lubridate_1.7.4 
-#[9] lifecycle_0.1.0  gtable_0.2.0     nlme_3.1-137     lattice_0.20-38 
-#[13] pkgconfig_2.0.2  rlang_0.4.1      cli_1.1.0        rstudioapi_0.10 
-#[17] haven_2.1.1      withr_2.1.2      xml2_1.2.2       httr_1.4.1      
-#[21] generics_0.0.2   vctrs_0.2.0      hms_0.5.2        grid_3.5.3      
-#[25] tidyselect_0.2.5 glue_1.3.1       R6_2.4.0         readxl_1.3.1    
-#[29] modelr_0.1.5     magrittr_1.5     backports_1.1.4  scales_1.0.0    
-#[33] rvest_0.3.4      assertthat_0.2.1 colorspace_1.4-0 stringi_1.3.1   
-#[37] lazyeval_0.2.1   munsell_0.5.0    broom_0.5.2      crayon_1.3.4  
+#  [1] Rcpp_1.0.3       cellranger_1.1.0 pillar_1.4.3     compiler_3.6.2  
+#[5] dbplyr_1.4.2     tools_3.6.2      jsonlite_1.6.1   lubridate_1.7.4 
+#[9] lifecycle_0.1.0  nlme_3.1-144     gtable_0.3.0     lattice_0.20-40 
+#[13] pkgconfig_2.0.3  rlang_0.4.4      reprex_0.3.0     cli_2.0.1       
+#[17] rstudioapi_0.11  DBI_1.1.0        haven_2.2.0      withr_2.1.2     
+#[21] xml2_1.2.2       httr_1.4.1       fs_1.3.1         generics_0.0.2  
+#[25] vctrs_0.2.2      hms_0.5.3        grid_3.6.2       tidyselect_1.0.0
+#[29] glue_1.3.1       R6_2.4.1         fansi_0.4.1      readxl_1.3.1    
+#[33] modelr_0.1.5     magrittr_1.5     backports_1.1.5  scales_1.1.0    
+#[37] rvest_0.3.5      assertthat_0.2.1 colorspace_1.4-1 utf8_1.1.4      
+#[41] stringi_1.4.6    lazyeval_0.2.2   munsell_0.5.0    broom_0.5.4     
+#[45] crayon_1.3.4 
