1128b7d89dad3e29ecd789cf36ee9ff8e2958466
laninsky <laninsky@gmail.com>
2020-12-23 14:42:44 +1300

    Tweaked name and added sesh info

--- chickadee/scripts/Fig_S7c_stationarity_convergence_results.R
+++ chickadee/scripts/Fig_S7c_stationarity_convergence_results.R
@@ -1,3 +1,6 @@
+# This code corresponds to Fig S7(c) in Alexander et al.
+# It analyses files from the bgc analysis represented in Fig_S7a_BGC_anslyses.sh
+
 # 1. Loading in required libraries
 library(tidyverse)
 library(gtable)
@@ -594,3 +597,29 @@ write_delim(output,"FigS10_positive_beta_SNPs.txt",col_names = TRUE)
 prop_genome_covered_by_Wagner_outliers <- (dim(Wagner_data)[1]*10000)/(1047.81*1000*1000)
 binom.test(5,671,prop_genome_covered_by_Wagner_outliers,alternative = "less")
 
+sessionInfo()
+#R version 3.6.3 (2020-02-29)
+#Platform: x86_64-apple-darwin15.6.0 (64-bit)
+#Running under: macOS Mojave 10.14.6
+#
+#Matrix products: default
+#BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib
+#LAPACK: /Library/Frameworks/R.framework/Versions/3.6/Resources/lib/libRlapack.dylib
+#
+#locale:
+#  [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+#
+#attached base packages:
+#  [1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+#
+#other attached packages:
+#  [1] ggrepel_0.8.1     DescTools_0.99.39 gtable_0.3.0      forcats_0.4.0     stringr_1.4.0     dplyr_1.0.2       purrr_0.3.3       readr_1.3.1       tidyr_1.0.2      
+#[10] tibble_3.0.4      ggplot2_3.3.2     tidyverse_1.3.0  
+#
+#loaded via a namespace (and not attached):
+#  [1] tidyselect_1.1.0  haven_2.2.0       lattice_0.20-38   colorspace_1.4-1  vctrs_0.3.4       generics_0.1.0    expm_0.999-4      rlang_0.4.8       e1071_1.7-3      
+#[10] pillar_1.4.3      glue_1.4.2        withr_2.1.2       DBI_1.1.0         dbplyr_1.4.2      modelr_0.1.5      readxl_1.3.1      rootSolve_1.8.2.1 lifecycle_0.2.0  
+#[19] munsell_0.5.0     cellranger_1.1.0  rvest_0.3.5       mvtnorm_1.1-1     lmom_2.8          class_7.3-15      fansi_0.4.1       broom_0.7.2       Rcpp_1.0.4.6     
+#[28] scales_1.1.0      backports_1.1.5   jsonlite_1.6.1    fs_1.3.1          gld_2.6.2         Exact_2.1         hms_0.5.3         stringi_1.4.5     cli_2.0.1        
+#[37] tools_3.6.3       magrittr_1.5      crayon_1.3.4      pkgconfig_2.0.3   ellipsis_0.3.0    MASS_7.3-51.5     Matrix_1.2-18     xml2_1.2.2        reprex_0.3.0     
+#[46] lubridate_1.7.4   assertthat_0.2.1  httr_1.4.1        rstudioapi_0.11   R6_2.4.1          boot_1.3-24       compiler_3.6.3 
